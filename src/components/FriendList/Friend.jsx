01d410b316f1bcc063b77c7d41597374fae66de0
Yuriy Krasyukov <89795245+wolk8506@users.noreply.github.com>
2022-05-28 11:51:23 +0300

    fix#4

--- src/components/FriendList/Friend.jsx
+++ src/components/FriendList/Friend.jsx
@@ -5,7 +5,7 @@ import s from 'components/FriendList/Friend.module.css'
 export const Friend = ({isOnline, avatar, name }) => {
     return (
       <Fragment>
-        {isOnline?<span className={s.statusOn}></span>:<span className={s.statusOff} ></span>}
+        <span className={isOnline ? s.statusOn:s.statusOff}></span>
         <span className={s.status}>{isOnline}</span>
         <img className={s.avatar} src={avatar} alt="User avatar" width="48" />
         <p className="name">{name}</p>
